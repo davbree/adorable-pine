a181f5b697a234ebc2db39fff3cfa0e1de252df5
Stackbit <projects@stackbit.com>
2019-10-06 10:31:42 +0300

    initial commit by Stackbit

---
+++ stackbit-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -v
+
+if [[ -z "${STACKBIT_API_KEY}" ]]; then
+    echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
+else
+    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://028eafff.ngrok.io/pull/5d99984d21ce7148b67ab0d6 
+fi
+hugo
+
